e30eb4a4aab7c7b60c2de84fd9d18b3fee0b6b75
dzimentey <dzimentey@gmail.com>
2022-04-01 16:28:42 +0300

    add: show ALL Posts at first page loading

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 import {useDispatch, useSelector} from "react-redux";
 import {AppRootStateType} from "./state/store";
 import {changeUserStatusAC, getLocalUsersAC, getUsersTC, UsersType} from "./state/users-reducer";
-import {PostsType} from "./state/all-posts-reducer";
+import {getAllPostsTC, PostsType} from "./state/all-posts-reducer";
 import {getFilteredPostsTC, removePostsAC} from "./state/filtered-posts-reducer";
 import {CommentsType, getCommentsTC} from "./state/coments-reducer";
 import {debounce} from "@mui/material";
@@ -14,7 +14,10 @@ function App() {
     const dispatch = useDispatch()
 
     useEffect(() => {
-        if (localStorage.getItem('localUsers') !== null) {
+
+        dispatch(getAllPostsTC())
+
+        if (localStorage.getItem('localUsers')) {
             getFromLocal()
         } else {
             dispatch(getUsersTC())
@@ -23,11 +26,11 @@ function App() {
     }, [])
 
     useEffect(() => {
-        localStorage.setItem('localUsers', JSON.stringify(users))
-    })
+      users.some(u => u.isChecked) ? localStorage.setItem('localUsers', JSON.stringify(users)) : localStorage.clear()
+       })
 
     const users = useSelector<AppRootStateType, UsersType>(state => state.users)
-    //const allPosts = useSelector<AppRootStateType, PostsType>(state => state.allPosts)
+    const allPosts = useSelector<AppRootStateType, PostsType>(state => state.allPosts)
     const filteredPosts = useSelector<AppRootStateType, PostsType>(state => state.filteredPosts)
     const comments = useSelector<AppRootStateType, CommentsType>(state => state.comments)
 
@@ -35,16 +38,15 @@ function App() {
         if (!isChecked) {
             dispatch(removePostsAC(id))
             dispatch(changeUserStatusAC(id, isChecked))
-        }
-        else {
+        } else {
             dispatch(getFilteredPostsTC(id))
             dispatch(changeUserStatusAC(id, isChecked))
         }
-    }, 300)
+    }, 500)
 
     const showCommentsHandler = debounce((postId: number) => {
-             dispatch(getCommentsTC(postId))
-    }, 300)
+        dispatch(getCommentsTC(postId))
+    }, 500)
 
     const getFromLocal = () => {
         const localUsersJSON = localStorage.getItem('localUsers')
@@ -58,29 +60,52 @@ function App() {
     return (
         <div className="App">
             <div className={'mainBlock'}>
+
                 <div className={'postsBlock'}>
+                    {
+                        users.some(u => u.isChecked)
+                            ?
+                            filteredPosts.map(post => {
+                                const onClickHandler = () => showCommentsHandler(post.id)
 
-                    {filteredPosts.map(post => {
-                        const onClickHandler = () => showCommentsHandler(post.id)
-
-                        return(
-                            <div key={post.id}>
-                                <li>{post.title}</li>
-                                <div style={{background: 'lightblue'}}>
-                                    {post.body}
-                                </div>
-                                <button onClick={onClickHandler}>expand</button>
-                                    <div style={{background: 'lightgray'}}>
-                                    {comments.map(c => c.postId === post.id ? <p key={c.id}> {c.id} {c.body}</p> : '')}
+                                return (
+                                    <div key={post.id}>
+                                        <li>{post.title}</li>
+                                        <div style={{background: 'lightblue'}}>
+                                            {post.body}
+                                        </div>
+                                        <button onClick={onClickHandler}>expand</button>
+                                        <div style={{background: 'lightgray'}}>
+                                            {comments.map(c => c.postId === post.id ?
+                                                <p key={c.id}> {c.id} {c.body}</p> : '')}
+                                        </div>
                                     </div>
-                            </div>
-                        )
-                    })}
+                                )
+                            })
+                            :
+                            allPosts.map(post => {
+                                const onClickHandler = () => showCommentsHandler(post.id)
+
+                                return (
+                                    <div key={post.id}>
+                                        <li>{post.title}</li>
+                                        <div style={{background: 'lightblue'}}>
+                                            {post.body}
+                                        </div>
+                                        <button onClick={onClickHandler}>expand</button>
+                                        <div style={{background: 'lightgray'}}>
+                                            {comments.map(c => c.postId === post.id ?
+                                                <p key={c.id}> {c.id} {c.body}</p> : '')}
+                                        </div>
+                                    </div>
+                                )
+                            })
+                    }
                 </div>
 
-                <div className={'selectBlock'} >
-                    <div className={'selectTitle'} >Select users to view their posts</div>
-                    <div className={'selectBody'} >
+                <div className={'selectBlock'}>
+                    <div className={'selectTitle'}>Select users to view their posts</div>
+                    <div className={'selectBody'}>
                         {users.map(u => {
                             const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
                                 let newIsChecked = e.currentTarget.checked
